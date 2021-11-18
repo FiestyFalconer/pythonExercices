034852d6e92ed2a0c0ac91b0d67c591d3669e8d6
FiestyFalconer <60130986+FiestyFalconer@users.noreply.github.com>
2021-11-18 15:02:13 +0100

    optimiser le code

--- connectionPython.py
+++ connectionPython.py
@@ -1,16 +1,30 @@
 import cv2
+import robomaster
 from robomaster import robot
 
-def cam():
+
+def main():
     ep_robot = robot.Robot()
+
     ep_robot.initialize(conn_type="ap")
+
+    version = ep_robot.get_vertion()
+    print("Robot version: {0}".format(version))
+
     ep_camera = ep_robot.camera
+
     ep_camera.start_video_stream(display=False)
-    for i in range(0, 999999):
+    for i in range(0, 200):
         img = ep_camera.read_cv2_image()
         cv2.imshow("Robot", img)
         cv2.waitKey(1)
     cv2.destroyAllWindows()
     ep_camera.stop_video_stream()
 
-cam()
+
+    ep_robot.close()
+
+if __name__ == "__main__":
+    main()
+    
+   
--- testRobo.py
+++ testRobo.py
@@ -11,6 +11,5 @@ ep_led.set_led(r=255, g=0, b=0)
 
 ep_version = ep_robot.get_version()
 
-
 print("Robot Version: {0}".format(ep_version))
 ep_robot.close()
